d9ecbc5d26995c15f9afc3cf0f8f1a477ac65cc5
Alison-Code <72189697+Alison-Code@users.noreply.github.com>
2021-10-01 12:31:16 +0530

    Update chess_board.py

--- chess_board.py
+++ chess_board.py
@@ -2,14 +2,16 @@ import turtle
 t = turtle.Turtle()
 t.speed(0)
 
+
 def box(ln):
     for i in range(4):
         t.forward(ln)
+        t.left(90)
 
-
-
+x = 0
 y = 0
-
+count = 2
+size = 50
 while True:
     t.goto(x, y)
     t.pendown()
@@ -17,6 +19,8 @@ while True:
     t.begin_fill()
     if count % 2 == 0:
         t.color("black", "black")
+    else:
+        t.color("black", "white")
     count += 1
     box(size)
     t.end_fill()
@@ -25,4 +29,8 @@ while True:
         t.penup()
         y += size
         count += 1
+    if y >= size * 8:
+        break
 
+t.hideturtle()
+turtle.done()
